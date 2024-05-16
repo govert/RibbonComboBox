3a57c247411054c726d22c90b9f4b628aca2760c
Govert van Drimmelen <govert@dnakode.com>
2024-05-16 11:56:59 +0200

    Add project files.

---
+++ AddIn.cs
@@ -0,0 +1,45 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.IntelliSense;
+
+namespace Test18
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            IntelliSenseServer.Install();
+        }
+        public void AutoClose()
+        {
+            IntelliSenseServer.Uninstall();
+        }
+    }
+
+    public static class Functions
+    {
+        [ExcelFunction(Name = "HelloWorld", Description = "Returns 'Hello, World!'")]
+        public static string HelloWorld()
+        {
+            return "Hello, World!";
+        }
+
+        [ExcelFunction(Name = "Add", Description = "Adds two numbers")]
+        public static double Add(
+            [ExcelArgument(Name = "First number", Description = "The first number")]
+            double a,
+            [ExcelArgument(Name = "Second number", Description = "The second number")]
+            double b)
+        {
+            return a + b;
+        }
+
+        // Add a "SayHello" function
+        [ExcelFunction(Name = "SayHello", Description = "Returns 'Hello, ' + name")]
+        public static string SayHello(
+            [ExcelArgument(Name = "Name", Description = "The name to say hello to")]
+            string name)
+        {
+            return "Hello, " + name;
+        }
+    }
+}
---
+++ Ribbon.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration.CustomUI;
+
+namespace Test18
+{
+    [ComVisible(true)]
+    public class Ribbon : ExcelRibbon
+    {
+        IRibbonUI _ribbon;
+
+        public override string GetCustomUI(string RibbonID)
+        {
+            return @"<customUI xmlns='http://schemas.microsoft.com/office/2009/07/customui' onLoad='OnLoad'>
+  <ribbon>
+    <tabs>
+      <tab id='MyTab' label='TestComboBox'>
+        <group id='MyGroup' label='My Group'>
+          <comboBox id='MyComboBox' 
+                    getItemCount='GetComboBoxItemCount'
+                    getItemLabel='GetComboBoxItemLabel'
+                    getText='GetComboBoxText'
+                    onChange='OnComboBoxTextChanged' />
+        </group>
+      </tab>
+    </tabs>
+  </ribbon>
+</customUI>";
+        }
+
+        // Implement OnLoad callback
+        public void OnLoad(IRibbonUI ribbon)
+        {
+            _ribbon = ribbon;
+            _comboBoxWrapper = new RibbonComboBoxWrapper(_ribbon, "MyComboBox");
+            _comboBoxWrapper.TextChanged += ComboBoxWrapper_TextChanged;
+            
+            // Example usage
+            _comboBoxWrapper.AddItem("Item 1");
+            _comboBoxWrapper.AddItem("Item 2");
+            _comboBoxWrapper.SetText("Select an item");
+        }
+
+        private void ComboBoxWrapper_TextChanged(object sender, string newText)
+        {
+            // Handle text changed event
+            System.Windows.Forms.MessageBox.Show($"ComboBox text changed to: {newText}");
+        }
+
+
+        #region Combobox wrapper
+        public RibbonComboBoxWrapper _comboBoxWrapper;
+        public RibbonComboBoxWrapper MyComboBox => _comboBoxWrapper;
+
+
+        public int GetComboBoxItemCount(IRibbonControl control)
+        {
+            return _comboBoxWrapper.GetItemCount(control);
+        }
+
+        public string GetComboBoxItemLabel(IRibbonControl control, int index)
+        {
+            return _comboBoxWrapper.GetItemLabel(control, index);
+        }
+
+        public string GetComboBoxText(IRibbonControl control)
+        {
+            return _comboBoxWrapper.GetComboBoxText(control);
+        }
+
+        public void OnComboBoxTextChanged(IRibbonControl control, string text)
+        {
+            _comboBoxWrapper.OnComboBoxTextChanged(control, text);
+        }
+        #endregion
+
+    }
+
+    public class RibbonComboBoxWrapper
+    {
+        private IRibbonUI _ribbon;
+        private string _controlId;
+
+        private List<string> items = new List<string>();
+        private string text;
+        public event EventHandler<string> TextChanged;
+
+        public RibbonComboBoxWrapper(IRibbonUI ribbon, string controlId)
+        {
+            _ribbon = ribbon;
+            _controlId = controlId;
+        }
+
+        public void AddItem(string item)
+        {
+            items.Add(item);
+            InvalidateControl();
+        }
+
+        public void SetItems(List<string> newItems)
+        {
+            items = newItems;
+            InvalidateControl();
+        }
+
+        public void SetText(string newText)
+        {
+            text = newText;
+            InvalidateControl();
+        }
+
+        public string GetText()
+        {
+            return text;
+        }
+
+        private void InvalidateControl()
+        {
+            // Invalidate the ComboBox control to force the Ribbon to call the callbacks again
+            _ribbon.InvalidateControl(_controlId);
+        }
+
+        public int GetItemCount(IRibbonControl control)
+        {
+            return items.Count;
+        }
+
+        public string GetItemLabel(IRibbonControl control, int index)
+        {
+            return items[index];
+        }
+
+        public string GetComboBoxText(IRibbonControl control)
+        {
+            return text;
+        }
+
+        public void OnComboBoxTextChanged(IRibbonControl control, string newText)
+        {
+            text = newText;
+            TextChanged?.Invoke(this, newText);
+        }
+    }
+}
